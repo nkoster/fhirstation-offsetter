bcfcc76c3a4e15b515e6cdcb866f09e63bae60ce
Niels Koster <n.koster@portavita.eu>
2021-08-04 14:18:05 +0200

    ATM-2771: Redirect to app

--- index.js
+++ index.js
@@ -48,11 +48,11 @@
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) {
-      return res.redirect('/')
+      return res.redirect('http://localhost:3000')
     }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
-        return res.redirect('/')
+        return res.redirect('http://localhost:3000')
       }
       req.user = user
       next()
